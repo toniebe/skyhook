a40d31403f825d6a9bb8b8e61c8c80a4c412856e
ebetoni <ahmad.zumaro@ptgue.com>
2024-06-24 10:21:35 +0700

    [fix]: change message content embrace provider

--- src/provider/Embrace.ts
+++ src/provider/Embrace.ts
@@ -21,7 +21,7 @@ export class Embrace extends DirectParseProvider {
             // Artifact not present
         }
         // const type = this.body.buildStatus
-        const content = 'Embrace Alert Testing'
+        const content = 'Embrace Alert: A new issue has been detected. Please investigate.'
         const download = this.body.downloadlink ?? 'https://embrace.io/'
         this.payload.username = projectName + ' Embrace IO'
         this.payload.avatar_url = 'https://play-lh.googleusercontent.com/JboJDdtKBMWmb039ZQhCjzuDiyKKSZjDbeY0o17yk2H-KL-u5cDeGbkpladlrxBgYIk=w240-h480-rw'
